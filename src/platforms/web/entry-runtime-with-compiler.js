29666239aa7f10b73b44f349fd3d452fe70f58bb
ru23 <fanqianweb@gmail.com>
2019-11-29 20:08:03 +0800

    代码提交

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -9,29 +9,44 @@ import { query } from './util/index'
 import { compileToFunctions } from './compiler/index'
 import { shouldDecodeNewlines, shouldDecodeNewlinesForHref } from './util/compat'
 
+/*
+  * aFn 函数会多次调用 里面就能体现了
+  *  用对象去缓存记录函数
+  *  idToTemplate 是一个函数，根据key值来 取值，如果第二次的key还是一样则从对象中取值，而不是重新在执行一次函数
+  * */
 const idToTemplate = cached(id => {
   const el = query(id)
   return el && el.innerHTML
 })
 
-const mount = Vue.prototype.$mount
+const mount = Vue.prototype.$mount //缓存上一次的Vue.prototype.$mount
+
+/*
+  * Vue 的$mount()为手动挂载，
+  * 在项目中可用于延时挂载（例如在挂载之前要进行一些其他操作、判断等），之后要手动挂载上。
+  * new Vue时，el和$mount并没有本质上的不同。
+  * @params
+  * */
 Vue.prototype.$mount = function (
-  el?: string | Element,
-  hydrating?: boolean
+  el?: string | Element, //真实dom 或者是string
+  hydrating?: boolean //新的虚拟dom vonde
 ): Component {
-  el = el && query(el)
+  el = el && query(el)//获取dom
 
   /* istanbul ignore if */
+  //如果el 是body 或者文档 则警告
   if (el === document.body || el === document.documentElement) {
     process.env.NODE_ENV !== 'production' && warn(
       `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
     )
     return this
   }
-
+  //获取参数
   const options = this.$options
   // resolve template/el and convert to render function
+  //解析模板/el并转换为render函数
   if (!options.render) {
+    //获取模板字符串
     let template = options.template
     if (template) {
       if (typeof template === 'string') {
